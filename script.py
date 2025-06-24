8cdcc6b8f188d9f8277b8d57d86c903934ebdf21
horaneq <szczepockak@gmail.com>
2025-06-25 00:39:46 +0200

    Added new Jupyter Notebook with final script

--- script.py
+++ script.py
@@ -12,7 +12,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
 from sklearn.utils import resample
 import openml
+import time
 
+plt.style.use('dark_background')
 scaler = StandardScaler()
 
 #    SWISS ROLL
@@ -26,36 +28,73 @@ ax.set_title("Swiss Roll - 3D")
 plt.show()
 
 pca_swiss = PCA(n_components=2)
+start_pca_swiss = time.perf_counter()
 X_pca_swiss = pca_swiss.fit_transform(X_swiss)
+end_pca_swiss = time.perf_counter()
 plt.scatter(X_pca_swiss[:,0], X_pca_swiss[:,1], c=y_swiss, cmap=plt.cm.viridis)
 plt.title("Swiss Roll - PCA 2D")
 plt.colorbar()
 plt.show()
 
+print(f"Czas wykonania PCA: {end_pca_swiss - start_pca_swiss:.5f} sekundy")
+
+
 tsne_swiss = TSNE(n_components=2, learning_rate='auto', init='random', perplexity=30, random_state=42, n_iter=250)
+start_tsne_swiss = time.perf_counter()
 X_tsne_swiss_iter = tsne_swiss.fit_transform(X_swiss)
+end_tsne_swiss = time.perf_counter()
+
+fig, axes = plt.subplots(2, 5, figsize=(20, 8))
 
 for i in range(1, 11):
     tsne_temp = TSNE(n_components=2, learning_rate='auto', init='random', perplexity=30, random_state=42, n_iter=i * 250)
-    X_tsne_swiss = tsne_temp.fit_transform(X_swiss) 
+    X_tsne_swiss = tsne_temp.fit_transform(X_swiss)
 
-    plt.figure(figsize=(8, 6))
-    plt.scatter(X_tsne_swiss[:, 0], X_tsne_swiss[:, 1], c=y_swiss, cmap=plt.cm.viridis)
-    plt.title(f"t-SNE po {i * 250} iteracjach")
-    plt.colorbar()
-    plt.show()
+    ax = axes[(i-1) // 5, (i-1) % 5] 
+    scatter = ax.scatter(X_tsne_swiss[:, 0], X_tsne_swiss[:, 1], c=y_swiss, cmap=plt.cm.viridis)
+    ax.set_title(f"t-SNE po {i * 250} iteracjach")
+    fig.colorbar(scatter, ax=ax)
+
+plt.tight_layout()
+plt.show()
+
 
 umap_swiss = umap.UMAP(n_components=2, n_neighbors=15, min_dist=0.1, random_state=42)
+start_umap_swiss = time.perf_counter()
+X_umap_swiss = umap_swiss.fit_transform(X_swiss)
+end_umap_swiss = time.perf_counter()
+
+fig, axes = plt.subplots(2, 5, figsize=(20, 8))
 
 for i in range(1, 11):
-    umap_temp = umap.UMAP(n_components=2, n_neighbors=i*5, min_dist=0.1, random_state=42)
+    umap_temp = umap.UMAP(n_components=2, n_neighbors=i * 5, min_dist=0.1, random_state=42)
     X_umap_swiss = umap_temp.fit_transform(X_swiss)
-    
-    plt.figure(figsize=(8, 6))
-    plt.scatter(X_umap_swiss[:, 0], X_umap_swiss[:, 1], c=y_swiss, cmap=plt.cm.viridis)
-    plt.title(f"UMAP dla {i*5} sąsiadów")
-    plt.colorbar()
-    plt.show()
+
+    ax = axes[(i-1) // 5, (i-1) % 5]  
+    scatter = ax.scatter(X_umap_swiss[:, 0], X_umap_swiss[:, 1], c=y_swiss, cmap=plt.cm.viridis)
+    ax.set_title(f"UMAP dla {i*5} sąsiadów")
+    fig.colorbar(scatter, ax=ax)
+
+plt.tight_layout()
+plt.show()
+
+
+times_swiss = {
+    'PCA': end_pca_swiss - start_pca_swiss,
+    't-SNE': end_tsne_swiss - start_tsne_swiss,
+    'UMAP': end_umap_swiss - start_umap_swiss
+}
+
+methods_swiss = list(times_swiss.keys())
+execution_times_swiss = list(times_swiss.values())
+
+plt.figure(figsize=(8, 6))
+plt.bar(methods_swiss, execution_times_swiss, color=['blue', 'green', 'red'])
+plt.xlabel('Metoda')
+plt.ylabel('Czas wykonania (sekundy)')
+plt.title('Porównanie czasu wykonania dla PCA, t-SNE, UMAP - zbiór Swiss Roll')
+plt.tight_layout()
+plt.show()
 
 #   BLOBS
 
@@ -68,24 +107,67 @@ ax.set_title("Blobs - 3D")
 plt.show()
 
 pca_blobs = PCA(n_components=2)
+start_pca_blobs = time.perf_counter()
 X_pca_blobs = pca_blobs.fit_transform(X_blobs)
+end_pca_blobs = time.perf_counter()
 plt.scatter(X_pca_blobs[:,0], X_pca_blobs[:,1], c=y_blobs, cmap=plt.cm.viridis)
 plt.title("Blobs - PCA 2D")
 plt.colorbar()
 plt.show()
 
 tsne_blobs = TSNE(n_components=2, learning_rate='auto', init='random', perplexity=30, random_state=42)
+start_tsne_blobs = time.perf_counter()
 X_tsne_blobs = tsne_blobs.fit_transform(X_blobs)
-plt.scatter(X_tsne_blobs[:,0], X_tsne_blobs[:,1], c=y_blobs, cmap=plt.cm.viridis)
-plt.title("Blobs - t-SNE 2D")
-plt.colorbar()
+end_tsne_blobs = time.perf_counter()
+
+fig, axes = plt.subplots(2, 5, figsize=(20, 8))
+
+for i in range(1, 11):
+    tsne_temp = TSNE(n_components=2, learning_rate='auto', init='random', perplexity=30, random_state=42, n_iter=i * 250)
+    X_tsne_blobs = tsne_temp.fit_transform(X_blobs)
+
+    ax = axes[(i-1) // 5, (i-1) % 5]
+    scatter = ax.scatter(X_tsne_blobs[:, 0], X_tsne_blobs[:, 1], c=y_blobs, cmap=plt.cm.viridis)
+    ax.set_title(f"t-SNE po {i * 250} iteracjach")
+    fig.colorbar(scatter, ax=ax)
+
+plt.tight_layout()
 plt.show()
 
 umap_blobs = umap.UMAP(n_components=2, n_neighbors=15, min_dist=0.1, random_state=42)
+start_umap_blobs = time.perf_counter()
 X_umap_blobs = umap_blobs.fit_transform(X_blobs)
-plt.scatter(X_umap_blobs[:,0], X_umap_blobs[:,1], c=y_blobs, cmap=plt.cm.viridis)
-plt.title("Blobs - UMAP 2D")
-plt.colorbar()
+end_umap_blobs = time.perf_counter()
+
+fig, axes = plt.subplots(2, 5, figsize=(20, 8))
+
+for i in range(1, 11):
+    umap_temp = umap.UMAP(n_components=2, n_neighbors=i * 5, min_dist=0.1, random_state=42)
+    X_umap_blobs = umap_temp.fit_transform(X_blobs)
+
+    ax = axes[(i-1) // 5, (i-1) % 5] 
+    scatter = ax.scatter(X_umap_blobs[:, 0], X_umap_blobs[:, 1], c=y_blobs, cmap=plt.cm.viridis)
+    ax.set_title(f"UMAP dla {i*5} sąsiadów")
+    fig.colorbar(scatter, ax=ax)
+
+plt.tight_layout()
+plt.show()
+
+times_blobs = {
+    'PCA': end_pca_blobs - start_pca_blobs,
+    't-SNE': end_tsne_blobs - start_tsne_blobs,
+    'UMAP': end_umap_blobs - start_umap_blobs
+}
+
+methods_blobs = list(times_blobs.keys())
+execution_times_blobs = list(times_blobs.values())
+
+plt.figure(figsize=(8, 6))
+plt.bar(methods_blobs, execution_times_blobs, color=['blue', 'green', 'red'])
+plt.xlabel('Metoda')
+plt.ylabel('Czas wykonania (sekundy)')
+plt.title('Porównanie czasu wykonania dla PCA, t-SNE, UMAP - zbiór Blobs')
+plt.tight_layout()
 plt.show()
 
 #    MNIST
@@ -102,8 +184,11 @@ X_test_scaled = scaler.transform(X_test)
 
 clf = MLPClassifier(hidden_layer_sizes=(100,), max_iter=300, random_state=42)
 
+start_nored_predict = time.perf_counter()
 clf.fit(X_train_scaled, y_train)
 y_pred = clf.predict(X_test_scaled)
+end_nored_predict = time.perf_counter()
+
 print("MNIST - Accuracy bez redukcji:", accuracy_score(y_test, y_pred))
 print("MNIST - Precision bez redukcji:", precision_score(y_test, y_pred, average='macro'))
 print("MNIST - Recall bez redukcji:", recall_score(y_test, y_pred, average='macro'))
@@ -123,8 +208,11 @@ X_train_pca, X_test_pca, y_train_pca, y_test_pca = train_test_split(X_mnist_pca,
 X_train_pca_scaled = scaler.fit_transform(X_train_pca)
 X_test_pca_scaled = scaler.transform(X_test_pca)
 
+start_pca_predict = time.perf_counter()
 clf.fit(X_train_pca_scaled, y_train_pca)
 y_pred_pca = clf.predict(X_test_pca_scaled)
+end_pca_predict = time.perf_counter()
+
 print("MNIST - Accuracy z PCA:", accuracy_score(y_test_pca, y_pred_pca))
 print("MNIST - Precision z PCA:", precision_score(y_test_pca, y_pred_pca, average='macro'))
 print("MNIST - Recall z PCA:", recall_score(y_test_pca, y_pred_pca, average='macro'))
@@ -144,8 +232,11 @@ X_train_tsne, X_test_tsne, y_train_tsne, y_test_tsne = train_test_split(X_mnist_
 X_train_tsne_scaled = scaler.fit_transform(X_train_tsne)
 X_test_tsne_scaled = scaler.transform(X_test_tsne)
 
+start_tsne_predict = time.perf_counter()
 clf.fit(X_train_tsne_scaled, y_train_tsne)
 y_pred_tsne = clf.predict(X_test_tsne_scaled)
+end_tsne_predict = time.perf_counter()
+
 print("MNIST - Accuracy z t-SNE:", accuracy_score(y_test_tsne, y_pred_tsne))
 print("MNIST - Precision z t-SNE:", precision_score(y_test_tsne, y_pred_tsne, average='macro'))
 print("MNIST - Recall z t-SNE:", recall_score(y_test_tsne, y_pred_tsne, average='macro'))
@@ -165,8 +256,11 @@ X_train_umap, X_test_umap, y_train_umap, y_test_umap = train_test_split(X_mnist_
 X_train_umap_scaled = scaler.fit_transform(X_train_umap)
 X_test_umap_scaled = scaler.transform(X_test_umap)
 
+start_umap_predict = time.perf_counter()
 clf.fit(X_train_umap_scaled, y_train_umap)
 y_pred_umap = clf.predict(X_test_umap_scaled)
+end_umap_predict = time.perf_counter()
+
 print("MNIST - Accuracy z UMAP:", accuracy_score(y_test_umap, y_pred_umap))
 print("MNIST - Precision z UMAP:", precision_score(y_test_umap, y_pred_umap, average='macro'))
 print("MNIST - Recall z UMAP:", recall_score(y_test_umap, y_pred_umap, average='macro'))
@@ -205,8 +299,26 @@ df_results = pd.DataFrame(results)
 df_results.set_index('Metoda', inplace=True)
 
 plt.figure(figsize=(8, 5))
-sns.heatmap(df_results, annot=True, cmap='YlGnBu', fmt=".3f")
+sns.heatmap(df_results, annot=True, cmap='YlGnBu', fmt=".3f", vmin=0.7, vmax=1)
 plt.title("Porównanie metryk klasyfikacji zbioru MNIST dla różnych metod redukcji")
+plt.tight_layout()
+plt.show()
+
+times = {
+    'Brak redukcji': end_nored_predict - start_nored_predict,
+    'PCA': end_pca_predict - start_pca_predict,
+    't-SNE': end_tsne_predict - start_tsne_predict,
+    'UMAP': end_umap_predict - start_umap_predict
+}
+
+methods = list(times.keys())
+execution_times = list(times.values())
+
+plt.figure(figsize=(8, 6))
+plt.bar(methods, execution_times, color=['purple', 'blue', 'green', 'red'])
+plt.xlabel('Metoda')
+plt.ylabel('Czas wykonania (sekundy)')
+plt.title('Porównanie czasu klasyfikacji zbioru MNIST dla różnych metod redukcji')
 plt.tight_layout()
 plt.show()
 
@@ -317,7 +429,7 @@ df_cov = pd.DataFrame(results_cov)
 df_cov.set_index('Metoda', inplace=True)
 
 plt.figure(figsize=(8, 5))
-sns.heatmap(df_cov, annot=True, cmap='Greens', fmt=".3f")
+sns.heatmap(df_cov, annot=True, cmap='Greens', fmt=".3f", vmin=0.2, vmax=1)
 plt.title("Porównanie metryk klasyfikacji zbioru Covertype dla różnych metod redukcji")
 plt.tight_layout()
 plt.show()
@@ -440,7 +552,7 @@ df_results = pd.DataFrame(results)
 df_results.set_index('Metoda', inplace=True)
 
 plt.figure(figsize=(8, 5))
-sns.heatmap(df_results, annot=True, cmap='YlGnBu', fmt=".3f")
+sns.heatmap(df_results, annot=True, cmap='YlGnBu', fmt=".3f", vmin=0.4, vmax=1)
 plt.title("Porównanie metryk klasyfikacji zbioru Heart Disease dla różnych metod redukcji")
 plt.tight_layout()
 plt.show()
