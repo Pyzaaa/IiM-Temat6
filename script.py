6cc853ff01ae7d68ec012b66268e0b9bbd99a256
horaneq <szczepockak@gmail.com>
2025-06-24 22:28:13 +0200

    Changed mice dataset to heart disease, additional visualizations for t-SNE and UMAP (change of shape with iterations/neighbors)

--- script.py
+++ script.py
@@ -11,10 +11,11 @@ from sklearn.metrics import precision_score, recall_score, f1_score, confusion_m
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
 from sklearn.utils import resample
+import openml
 
 scaler = StandardScaler()
 
-#    WIZUALIZACJE
+#    SWISS ROLL
 
 X_swiss, y_swiss = make_swiss_roll(n_samples=1000, noise=0.05)
 fig = plt.figure(figsize=(8,6))
@@ -31,19 +32,32 @@ plt.title("Swiss Roll - PCA 2D")
 plt.colorbar()
 plt.show()
 
-tsne_swiss = TSNE(n_components=2, learning_rate='auto', init='random', perplexity=30, random_state=42)
-X_tsne_swiss = tsne_swiss.fit_transform(X_swiss)
-plt.scatter(X_tsne_swiss[:,0], X_tsne_swiss[:,1], c=y_swiss, cmap=plt.cm.viridis)
-plt.title("Swiss Roll - t-SNE 2D")
-plt.colorbar()
-plt.show()
+tsne_swiss = TSNE(n_components=2, learning_rate='auto', init='random', perplexity=30, random_state=42, n_iter=250)
+X_tsne_swiss_iter = tsne_swiss.fit_transform(X_swiss)
+
+for i in range(1, 11):
+    tsne_temp = TSNE(n_components=2, learning_rate='auto', init='random', perplexity=30, random_state=42, n_iter=i * 250)
+    X_tsne_swiss = tsne_temp.fit_transform(X_swiss) 
+
+    plt.figure(figsize=(8, 6))
+    plt.scatter(X_tsne_swiss[:, 0], X_tsne_swiss[:, 1], c=y_swiss, cmap=plt.cm.viridis)
+    plt.title(f"t-SNE po {i * 250} iteracjach")
+    plt.colorbar()
+    plt.show()
 
 umap_swiss = umap.UMAP(n_components=2, n_neighbors=15, min_dist=0.1, random_state=42)
-X_umap_swiss = umap_swiss.fit_transform(X_swiss)
-plt.scatter(X_umap_swiss[:,0], X_umap_swiss[:,1], c=y_swiss, cmap=plt.cm.viridis)
-plt.title("Swiss Roll - UMAP 2D")
-plt.colorbar()
-plt.show()
+
+for i in range(1, 11):
+    umap_temp = umap.UMAP(n_components=2, n_neighbors=i*5, min_dist=0.1, random_state=42)
+    X_umap_swiss = umap_temp.fit_transform(X_swiss)
+    
+    plt.figure(figsize=(8, 6))
+    plt.scatter(X_umap_swiss[:, 0], X_umap_swiss[:, 1], c=y_swiss, cmap=plt.cm.viridis)
+    plt.title(f"UMAP dla {i*5} sąsiadów")
+    plt.colorbar()
+    plt.show()
+
+#   BLOBS
 
 X_blobs, y_blobs = make_blobs(n_samples=1000, centers=4, n_features=3, random_state=0)
 fig = plt.figure(figsize=(8,6))
@@ -90,11 +104,11 @@ clf = MLPClassifier(hidden_layer_sizes=(100,), max_iter=300, random_state=42)
 
 clf.fit(X_train_scaled, y_train)
 y_pred = clf.predict(X_test_scaled)
-print("Accuracy no reduction:", accuracy_score(y_test, y_pred))
-print("Precision no reduction:", precision_score(y_test, y_pred, average='macro'))
-print("Recall no reduction:", recall_score(y_test, y_pred, average='macro'))
-print("F1-score no reduction:", f1_score(y_test, y_pred, average='macro'))
-print("Confusion Matrix no reduction:\n", confusion_matrix(y_test, y_pred))
+print("MNIST - Accuracy bez redukcji:", accuracy_score(y_test, y_pred))
+print("MNIST - Precision bez redukcji:", precision_score(y_test, y_pred, average='macro'))
+print("MNIST - Recall bez redukcji:", recall_score(y_test, y_pred, average='macro'))
+print("MNIST - F1-score bez redukcji:", f1_score(y_test, y_pred, average='macro'))
+print("MNIST - Confusion Matrix bez redukcji:\n", confusion_matrix(y_test, y_pred))
 
 pca_mnist = PCA(n_components=50)
 X_mnist_pca = pca_mnist.fit_transform(X_mnist)
@@ -111,11 +125,11 @@ X_test_pca_scaled = scaler.transform(X_test_pca)
 
 clf.fit(X_train_pca_scaled, y_train_pca)
 y_pred_pca = clf.predict(X_test_pca_scaled)
-print("Accuracy with PCA:", accuracy_score(y_test_pca, y_pred_pca))
-print("Precision with PCA:", precision_score(y_test_pca, y_pred_pca, average='macro'))
-print("Recall with PCA:", recall_score(y_test_pca, y_pred_pca, average='macro'))
-print("F1-score with PCA:", f1_score(y_test_pca, y_pred_pca, average='macro'))
-print("Confusion Matrix with PCA:\n", confusion_matrix(y_test_pca, y_pred_pca))
+print("MNIST - Accuracy z PCA:", accuracy_score(y_test_pca, y_pred_pca))
+print("MNIST - Precision z PCA:", precision_score(y_test_pca, y_pred_pca, average='macro'))
+print("MNIST - Recall z PCA:", recall_score(y_test_pca, y_pred_pca, average='macro'))
+print("MNIST - F1-score z PCA:", f1_score(y_test_pca, y_pred_pca, average='macro'))
+print("MNIST - Confusion Matrix z PCA:\n", confusion_matrix(y_test_pca, y_pred_pca))
 
 tsne_mnist = TSNE(n_components=2, learning_rate='auto', init='random', perplexity=30, random_state=42)
 X_mnist_tsne = tsne_mnist.fit_transform(X_mnist)
@@ -132,11 +146,11 @@ X_test_tsne_scaled = scaler.transform(X_test_tsne)
 
 clf.fit(X_train_tsne_scaled, y_train_tsne)
 y_pred_tsne = clf.predict(X_test_tsne_scaled)
-print("Accuracy with t-SNE:", accuracy_score(y_test_tsne, y_pred_tsne))
-print("Precision with t-SNE:", precision_score(y_test_tsne, y_pred_tsne, average='macro'))
-print("Recall with t-SNE:", recall_score(y_test_tsne, y_pred_tsne, average='macro'))
-print("F1-score with t-SNE:", f1_score(y_test_tsne, y_pred_tsne, average='macro'))
-print("Confusion Matrix with t-SNE:\n", confusion_matrix(y_test_tsne, y_pred_tsne))
+print("MNIST - Accuracy z t-SNE:", accuracy_score(y_test_tsne, y_pred_tsne))
+print("MNIST - Precision z t-SNE:", precision_score(y_test_tsne, y_pred_tsne, average='macro'))
+print("MNIST - Recall z t-SNE:", recall_score(y_test_tsne, y_pred_tsne, average='macro'))
+print("MNIST - F1-score z t-SNE:", f1_score(y_test_tsne, y_pred_tsne, average='macro'))
+print("MNIST - Confusion Matrix z t-SNE:\n", confusion_matrix(y_test_tsne, y_pred_tsne))
 
 umap_mnist = umap.UMAP(n_components=50, n_neighbors=15, min_dist=0.1, random_state=42)
 X_mnist_umap = umap_mnist.fit_transform(X_mnist)
@@ -153,11 +167,11 @@ X_test_umap_scaled = scaler.transform(X_test_umap)
 
 clf.fit(X_train_umap_scaled, y_train_umap)
 y_pred_umap = clf.predict(X_test_umap_scaled)
-print("Accuracy with UMAP:", accuracy_score(y_test_umap, y_pred_umap))
-print("Precision with UMAP:", precision_score(y_test_umap, y_pred_umap, average='macro'))
-print("Recall with UMAP:", recall_score(y_test_umap, y_pred_umap, average='macro'))
-print("F1-score with UMAP:", f1_score(y_test_umap, y_pred_umap, average='macro'))
-print("Confusion Matrix with UMAP:\n", confusion_matrix(y_test_umap, y_pred_umap))
+print("MNIST - Accuracy z UMAP:", accuracy_score(y_test_umap, y_pred_umap))
+print("MNIST - Precision z UMAP:", precision_score(y_test_umap, y_pred_umap, average='macro'))
+print("MNIST - Recall z UMAP:", recall_score(y_test_umap, y_pred_umap, average='macro'))
+print("MNIST - F1-score z UMAP:", f1_score(y_test_umap, y_pred_umap, average='macro'))
+print("MNIST - Confusion Matrix z UMAP:\n", confusion_matrix(y_test_umap, y_pred_umap))
 
 results = {
     'Metoda': ['Brak redukcji', 'PCA', 't-SNE', 'UMAP'],
@@ -192,7 +206,7 @@ df_results.set_index('Metoda', inplace=True)
 
 plt.figure(figsize=(8, 5))
 sns.heatmap(df_results, annot=True, cmap='YlGnBu', fmt=".3f")
-plt.title("Porównanie metryk klasyfikacji dla różnych metod redukcji")
+plt.title("Porównanie metryk klasyfikacji zbioru MNIST dla różnych metod redukcji")
 plt.tight_layout()
 plt.show()
 
@@ -203,15 +217,20 @@ X_cov, y_cov = data['data'], data['target']
 X_cov, y_cov = resample(X_cov, y_cov, n_samples=10000, random_state=42)
 
 X_train, X_test, y_train, y_test = train_test_split(X_cov, y_cov, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
+clf = MLPClassifier(hidden_layer_sizes=(100,), max_iter=300, random_state=42)
+
 clf.fit(X_train_scaled, y_train)
 y_pred = clf.predict(X_test_scaled)
-print("COVERTYPE - Accuracy no reduction:", accuracy_score(y_test, y_pred))
-print("COVERTYPE - Precision:", precision_score(y_test, y_pred, average='macro'))
-print("COVERTYPE - Recall:", recall_score(y_test, y_pred, average='macro'))
-print("COVERTYPE - F1-score:", f1_score(y_test, y_pred, average='macro'))
+
+print("COVERTYPE - Accuracy bez redukcji:", accuracy_score(y_test, y_pred))
+print("COVERTYPE - Precision bez redukcji:", precision_score(y_test, y_pred, average='macro'))
+print("COVERTYPE - Recall bez redukcji:", recall_score(y_test, y_pred, average='macro'))
+print("COVERTYPE - F1-score bez redukcji:", f1_score(y_test, y_pred, average='macro'))
 
 pca_cov = PCA(n_components=50)
 X_cov_pca = pca_cov.fit_transform(X_cov)
@@ -221,21 +240,50 @@ X_test_pca_scaled = scaler.transform(X_test_pca)
 clf.fit(X_train_pca_scaled, y_train_pca)
 y_pred_pca = clf.predict(X_test_pca_scaled)
 
+print("COVERTYPE - Accuracy z PCA:", accuracy_score(y_test_pca, y_pred_pca))
+print("COVERTYPE - Precision z PCA:", precision_score(y_test_pca, y_pred_pca, average='macro'))
+print("COVERTYPE - Recall z PCA:", recall_score(y_test_pca, y_pred_pca, average='macro'))
+print("COVERTYPE - F1-score z PCA:", f1_score(y_test_pca, y_pred_pca, average='macro'))
+
 tsne_cov = TSNE(n_components=2, learning_rate='auto', init='random', perplexity=30, random_state=42)
 X_cov_tsne = tsne_cov.fit_transform(X_cov)
+plt.figure(figsize=(8, 6))
+plt.scatter(X_cov_tsne[:, 0], X_cov_tsne[:, 1], c=y_cov, cmap='tab10', s=10)
+plt.title("Covertype - t-SNE 2D")
+plt.colorbar(label='Typ pokrycia lasu')
+plt.tight_layout()
+plt.show()
+
 X_train_tsne, X_test_tsne, y_train_tsne, y_test_tsne = train_test_split(X_cov_tsne, y_cov, test_size=0.2, random_state=42)
 X_train_tsne_scaled = scaler.fit_transform(X_train_tsne)
 X_test_tsne_scaled = scaler.transform(X_test_tsne)
 clf.fit(X_train_tsne_scaled, y_train_tsne)
 y_pred_tsne = clf.predict(X_test_tsne_scaled)
 
+print("COVERTYPE - Accuracy z t-SNE:", accuracy_score(y_test_tsne, y_pred_tsne))
+print("COVERTYPE - Precision z t-SNE:", precision_score(y_test_tsne, y_pred_tsne, average='macro'))
+print("COVERTYPE - Recall z t-SNE:", recall_score(y_test_tsne, y_pred_tsne, average='macro'))
+print("COVERTYPE - F1-score z t-SNE:", f1_score(y_test_tsne, y_pred_tsne, average='macro'))
+
 umap_cov = umap.UMAP(n_components=50, n_neighbors=15, min_dist=0.1, random_state=42)
 X_cov_umap = umap_cov.fit_transform(X_cov)
+plt.figure(figsize=(8, 6))
+plt.scatter(X_cov_umap[:, 0], X_cov_umap[:, 1], c=y_cov, cmap='tab10', s=10)
+plt.title("Covertype - UMAP 2D")
+plt.colorbar(label='Typ pokrycia lasu')
+plt.tight_layout()
+plt.show()
+
 X_train_umap, X_test_umap, y_train_umap, y_test_umap = train_test_split(X_cov_umap, y_cov, test_size=0.2, random_state=42)
 X_train_umap_scaled = scaler.fit_transform(X_train_umap)
 X_test_umap_scaled = scaler.transform(X_test_umap)
 clf.fit(X_train_umap_scaled, y_train_umap)
 y_pred_umap = clf.predict(X_test_umap_scaled)
+
+print("COVERTYPE - Accuracy z UMAP:", accuracy_score(y_test_umap, y_pred_umap))
+print("COVERTYPE - Precision z UMAP:", precision_score(y_test_umap, y_pred_umap, average='macro'))
+print("COVERTYPE - Recall z UMAP:", recall_score(y_test_umap, y_pred_umap, average='macro'))
+print("COVERTYPE - F1-score z UMAP:", f1_score(y_test_umap, y_pred_umap, average='macro'))
 
 results_cov = {
     'Metoda': ['Brak redukcji', 'PCA', 't-SNE', 'UMAP'],
@@ -270,56 +318,97 @@ df_cov.set_index('Metoda', inplace=True)
 
 plt.figure(figsize=(8, 5))
 sns.heatmap(df_cov, annot=True, cmap='Greens', fmt=".3f")
-plt.title("COVERTYPE - porównanie metryk klasyfikacji")
+plt.title("Porównanie metryk klasyfikacji zbioru Covertype dla różnych metod redukcji")
 plt.tight_layout()
 plt.show()
 
-#   MICE PROTEIN
+#   HEART DISEASE
 
-data = fetch_openml(name='MiceProtein', version=4, as_frame=True)
-df = data.frame.dropna()
+heart_disease = openml.datasets.get_dataset(22)
+X, y, _, _ = heart_disease.get_data(target=heart_disease.default_target_attribute)
 
-X = df.select_dtypes(include='number')
-y = df['class']
+y = pd.factorize(y)[0]
 
-X, y = resample(X, y, n_samples=min(1000, len(X)), random_state=42, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
+clf = MLPClassifier(hidden_layer_sizes=(100,), max_iter=300, random_state=42)
+
 clf.fit(X_train_scaled, y_train)
 y_pred = clf.predict(X_test_scaled)
-print("MICE - Accuracy no reduction:", accuracy_score(y_test, y_pred))
-print("MICE - Precision:", precision_score(y_test, y_pred, average='macro'))
-print("MICE - Recall:", recall_score(y_test, y_pred, average='macro'))
-print("MICE - F1-score:", f1_score(y_test, y_pred, average='macro'))
+print("HEART DISEASE - Accuracy bez redukcji:", accuracy_score(y_test, y_pred))
+print("HEART DISEASE - Precision bez redukcji:", precision_score(y_test, y_pred, average='macro'))
+print("HEART DISEASE - Recall bez redukcji:", recall_score(y_test, y_pred, average='macro'))
+print("HEART DISEASE - F1-score bez redukcji:", f1_score(y_test, y_pred, average='macro'))
+print("HEART DISEASE - Confusion Matrix bez redukcji:\n", confusion_matrix(y_test, y_pred))
 
-pca_mice = PCA(n_components=10)
-X_mice_pca = pca_mice.fit_transform(X)
-X_train_pca, X_test_pca, y_train_pca, y_test_pca = train_test_split(X_mice_pca, y, test_size=0.2, random_state=42, stratify=y)
+pca_heart = PCA(n_components=2)
+X_heart_pca = pca_heart.fit_transform(X)
+plt.figure(figsize=(8, 6))
+plt.scatter(X_heart_pca[:, 0], X_heart_pca[:, 1], c=y, cmap='tab10', s=10)
+plt.title("Heart Disease - PCA 2D")
+plt.colorbar(label='Choroba serca')
+plt.tight_layout()
+plt.show()
+
+X_train_pca, X_test_pca, y_train_pca, y_test_pca = train_test_split(X_heart_pca, y, test_size=0.2, random_state=42)
 X_train_pca_scaled = scaler.fit_transform(X_train_pca)
 X_test_pca_scaled = scaler.transform(X_test_pca)
+
 clf.fit(X_train_pca_scaled, y_train_pca)
 y_pred_pca = clf.predict(X_test_pca_scaled)
+print("HEART DISEASE - Accuracy z PCA:", accuracy_score(y_test_pca, y_pred_pca))
+print("HEART DISEASE - Precision z PCA:", precision_score(y_test_pca, y_pred_pca, average='macro'))
+print("HEART DISEASE - Recall z PCA:", recall_score(y_test_pca, y_pred_pca, average='macro'))
+print("HEART DISEASE - F1-score z PCA:", f1_score(y_test_pca, y_pred_pca, average='macro'))
+print("HEART DISEASE - Confusion Matrix z PCA:\n", confusion_matrix(y_test_pca, y_pred_pca))
 
-tsne_mice = TSNE(n_components=2, learning_rate='auto', init='random', perplexity=30, random_state=42)
-X_mice_tsne = tsne_mice.fit_transform(X)
-X_train_tsne, X_test_tsne, y_train_tsne, y_test_tsne = train_test_split(X_mice_tsne, y, test_size=0.2, random_state=42, stratify=y)
+tsne_heart = TSNE(n_components=2, learning_rate='auto', init='random', perplexity=30, random_state=42)
+X_heart_tsne = tsne_heart.fit_transform(X)
+plt.figure(figsize=(8, 6))
+plt.scatter(X_heart_tsne[:, 0], X_heart_tsne[:, 1], c=y, cmap='tab10', s=10)
+plt.title("Heart Disease - t-SNE 2D")
+plt.colorbar(label='Choroba serca')
+plt.tight_layout()
+plt.show()
+
+X_train_tsne, X_test_tsne, y_train_tsne, y_test_tsne = train_test_split(X_heart_tsne, y, test_size=0.2, random_state=42)
 X_train_tsne_scaled = scaler.fit_transform(X_train_tsne)
 X_test_tsne_scaled = scaler.transform(X_test_tsne)
+
 clf.fit(X_train_tsne_scaled, y_train_tsne)
 y_pred_tsne = clf.predict(X_test_tsne_scaled)
+print("HEART DISEASE - Accuracy z t-SNE:", accuracy_score(y_test_tsne, y_pred_tsne))
+print("HEART DISEASE - Precision z t-SNE:", precision_score(y_test_tsne, y_pred_tsne, average='macro'))
+print("HEART DISEASE - Recall z t-SNE:", recall_score(y_test_tsne, y_pred_tsne, average='macro'))
+print("HEART DISEASE - F1-score z t-SNE:", f1_score(y_test_tsne, y_pred_tsne, average='macro'))
+print("HEART DISEASE - Confusion Matrix z t-SNE:\n", confusion_matrix(y_test_tsne, y_pred_tsne))
 
-umap_mice = umap.UMAP(n_components=10, n_neighbors=15, min_dist=0.1, random_state=42)
-X_mice_umap = umap_mice.fit_transform(X)
-X_train_umap, X_test_umap, y_train_umap, y_test_umap = train_test_split(X_mice_umap, y, test_size=0.2, random_state=42, stratify=y)
+umap_heart = umap.UMAP(n_components=2, n_neighbors=15, min_dist=0.1, random_state=42)
+X_heart_umap = umap_heart.fit_transform(X)
+plt.figure(figsize=(8, 6))
+plt.scatter(X_heart_umap[:, 0], X_heart_umap[:, 1], c=y, cmap='tab10', s=10)
+plt.title("Heart Disease - UMAP 2D")
+plt.colorbar(label='Choroba serca')
+plt.tight_layout()
+plt.show()
+
+X_train_umap, X_test_umap, y_train_umap, y_test_umap = train_test_split(X_heart_umap, y, test_size=0.2, random_state=42)
 X_train_umap_scaled = scaler.fit_transform(X_train_umap)
 X_test_umap_scaled = scaler.transform(X_test_umap)
+
 clf.fit(X_train_umap_scaled, y_train_umap)
 y_pred_umap = clf.predict(X_test_umap_scaled)
+print("HEART DISEASE - Accuracy z UMAP:", accuracy_score(y_test_umap, y_pred_umap))
+print("HEART DISEASE - Precision z UMAP:", precision_score(y_test_umap, y_pred_umap, average='macro'))
+print("HEART DISEASE - Recall z UMAP:", recall_score(y_test_umap, y_pred_umap, average='macro'))
+print("HEART DISEASE - F1-score z UMAP:", f1_score(y_test_umap, y_pred_umap, average='macro'))
+print("HEART DISEASE - Confusion Matrix z UMAP:\n", confusion_matrix(y_test_umap, y_pred_umap))
 
-results_mice = {
+results = {
     'Metoda': ['Brak redukcji', 'PCA', 't-SNE', 'UMAP'],
     'Accuracy': [
         accuracy_score(y_test, y_pred),
@@ -347,11 +436,11 @@ results_mice = {
     ]
 }
 
-df_mice = pd.DataFrame(results_mice)
-df_mice.set_index('Metoda', inplace=True)
+df_results = pd.DataFrame(results)
+df_results.set_index('Metoda', inplace=True)
 
 plt.figure(figsize=(8, 5))
-sns.heatmap(df_mice, annot=True, cmap='coolwarm', fmt=".3f")
-plt.title("MICE PROTEIN - porównanie metryk klasyfikacji")
+sns.heatmap(df_results, annot=True, cmap='YlGnBu', fmt=".3f")
+plt.title("Porównanie metryk klasyfikacji zbioru Heart Disease dla różnych metod redukcji")
 plt.tight_layout()
 plt.show()
